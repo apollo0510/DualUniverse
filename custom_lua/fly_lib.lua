2bb0fd6f655a9b46cdb5008db6035fbdcefbf0e2
Olaf Reusch <apollo0510@hotmail.com>
2021-11-29 16:29:00 +0100

    working on fly lib integration

--- custom_lua/fly_lib.lua
+++ custom_lua/fly_lib.lua
@@ -2,10 +2,12 @@ local FlyLib=
 {
     UnitClasses=
     {
-        CoreUnit         = { need = 1; meta = nil; },
-        ScreenUnit       = { need = 1; meta = nil; },
-        GyroUnit         = { need = 1; meta = nil; },
-        TelemeterUnit    = { need = 0; meta = nil; },
+        CoreUnit           = { need = 1; meta = nil; name="core"; },
+        ScreenUnit         = { need = 1; meta = nil; name="screen"; },
+        GyroUnit           = { need = 1; meta = nil; name="gyro"; },
+        TelemeterUnit      = { need = 0; meta = nil; name="tele"; },
+        SpaceFuelContainer = { need = 0; meta = nil; },
+        CockpitCommandmentUnit = { need = 0; meta = nil; },
     };
 
     ClassTranslation=
@@ -21,16 +23,26 @@ local FlyLib=
     system = nil;
     unit   = nil;
     core   = nil;
+    gyro   = nil;
+    tele   = nil;
     screen = nil;
 
     InitOk = false;
+
+    fps_count = 0;
+    t         = 0;
+    t_10hz    = 0;
+    t_1hz     = 0;
+    fps       = 0;
 };
+
+local format=string.format;
 
 -- ******************************************************************
 --
 -- ******************************************************************
 
-function FlyLib:ErrorHandler(text)
+ function FlyLib:ErrorHandler(text)
     local u=self.UnitClasses;
     local screen=u.ScreenUnit[1];
     if screen then
@@ -44,6 +56,19 @@ end
 --
 -- ******************************************************************
 
+function FlyLib:SecureCall(func_name,a1,a2,a3)
+    local f = self[func_name];
+    local ok, message = pcall(f,self,a1,a2,a3);
+    if not ok then
+        local text= format("Error in %s :\n%s",func_name,message);
+        self:ErrorHandler(string.gsub(text,"\n", "<br>"));
+    end
+end
+
+    -- ******************************************************************
+    --
+    -- ******************************************************************
+
 function FlyLib:IdentifySlots(system,unit)
     self.system=system;
     self.unit  =unit;
@@ -56,13 +81,17 @@ function FlyLib:IdentifySlots(system,unit)
             class =  self.ClassTranslation[class] or class;
             local class_table = u[class];
             if class_table then
-                    local unit = {};
-                    unit.obj=obj; 
-                    setmetatable(unit,class_table.meta);
-                    class_table[#class_table+1]=unit; 
-                    unit.id=obj.getId();
-                else if class~="Generic" then
-               	self.system.print(format("Unexpected unit class %s",class));
+                local unit = {};
+                unit.obj=obj; 
+                setmetatable(unit,class_table.meta);
+                class_table[#class_table+1]=unit; 
+                unit.id=obj.getId();
+                if class_table.name then
+                    self[class_table.name]=obj;
+                end
+            else 
+                if class~="Generic" then
+           	        self.system.print(format("Unexpected unit class %s",class));
                 end    
             end    
         end    
@@ -76,29 +105,64 @@ function FlyLib:IdentifySlots(system,unit)
         end    
     end    
     -- ******************************************************************
-    if u.CoreUnit then
-        local core_entry  = u.CoreUnit[1]; 
-        if core_entry then
-            self.core = core_entry.obj;
-        end
-    end    
-    if u.ScreenUnit then
-        local screen_entry  = u.ScreenUnit[1]; 
-        if screen_entry then
-            self.screen = screen_entry.obj;
-        end
-    end    
-    -- ******************************************************************
+    if self.InitOk then
+        self.screen.setCenteredText("Ready");        
+    end
     return self.InitOk;
 end
 
--- ******************************************************************
---
--- ******************************************************************
+    -- ******************************************************************
+    --
+    -- ******************************************************************
 
+function FlyLib:OnPeriodic()
 
+    local core        = self.core;
+    local unit        = self.unit;
+    local v_velo = vec3(core.getVelocity());
+    local speed  = v_velo:len();
+    local kmh    = speed * 3.6;
+    local gear_down = unit.isAnyLandingGearExtended();
+    if (kmh >= (GearSpeed + 50.0)) and (gear_down==1) then
+        --print("Auto closing gear " );
+        unit.retractLandingGears();
+    end   
+    if (kmh <= (GearSpeed - 50.0)) and (gear_down==0) then
+        --print("Auto deploying gear ");
+        unit.extendLandingGears();    
+    end   
+end
 
+    -- ******************************************************************
+    --
+    -- ******************************************************************
 
+function FlyLib:OnUpdate()
+    
+    if not self.InitOk then return;
 
+    local core        = self.core;
+    local unit        = self.unit;
+    local system      = self.system;
+
+    local t           = system.getTime();
+    self.t            = t;
+    self.fps_count    = self.fps_count+1;
+
+    local draw_10hz=false;
+    local draw_1hz =false;
+
+    if (t - self.t_10hz) >= 0.1 then
+        self.t_10hz = t;
+        draw_10hz = true;
+        if (t - self.t_1hz) >= 1.0 then
+            self.t_1hz = t;
+            draw_1hz = true;
+            self.fps = self.fps_count;
+            self.fps_count = 0;
+        end
+    end
+
+end
 
 return FlyLib;
