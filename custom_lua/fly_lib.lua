58351e6ccbcf022e847cc5fadec3c5dcaffc6dd6
Olaf Reusch <apollo0510@hotmail.com>
2021-11-30 11:37:17 +0100

    loading again.

--- custom_lua/fly_lib.lua
+++ custom_lua/fly_lib.lua
@@ -5,7 +5,7 @@ local FlyLib=
         CoreUnit           = { need = 1; meta = nil; name="core"; },
         ScreenUnit         = { need = 1; meta = nil; name="screen"; },
         GyroUnit           = { need = 1; meta = nil; name="gyro"; },
-        TelemeterUnit      = { need = 0; meta = nil; name="tele"; },
+        TelemeterUnit      = { need = 0; meta = nil; name="telemeter"; },
         SpaceFuelContainer = { need = 0; meta = nil; },
         CockpitCommandmentUnit = { need = 0; meta = nil; },
     };
@@ -34,9 +34,34 @@ local FlyLib=
     t_10hz    = 0;
     t_1hz     = 0;
     fps       = 0;
+
+    v_forward = nil;
+    v_up      = nil;
+    v_right   = nil;
+
+    ground_distance = -1;
+
+    player_rel_pos = nil;
+    player_distance = 0.0;
+    altitude = 0.0;
+    atmosphere = 0.0;
+    planetinfluence = 0.0;
+    kmh = 0.0;
+
+    v_angle_v = 0.0;
+    v_angle_h = 0.0;
+    v_angle_valid = false;
 };
 
-local format=string.format;
+-- ******************************************************************
+--
+-- ******************************************************************
+
+local degree_to_delta = 4.0;
+local rad_to_delta    = 4.0 * 180.0 / math.pi;
+local format          = string.format;
+local acos            = math.acos;
+local pi_half         = math.pi / 2.0;
 
 -- ******************************************************************
 --
@@ -145,6 +170,19 @@ function FlyLib:OnUpdate()
    local core        = self.core;
    local unit        = self.unit;
    local system      = self.system;
+   local gyro        = self.gyro;
+
+   if self.v_forward == nil then
+       self.v_forward = vec3(core.getConstructOrientationForward());
+       self.v_up      = vec3(core.getConstructOrientationUp());
+       self.v_right   = vec3(core.getConstructOrientationRight());
+   end
+
+   if self.telemeter then
+       self.ground_distance = self.telemeter.getDistance();
+   else
+       self.ground_distance = -1;
+   end    
 
    local t           = system.getTime();
    self.t            = t;
@@ -163,6 +201,47 @@ function FlyLib:OnUpdate()
            self.fps_count = 0;
        end
    end
+
+   self.player_rel_pos  = vec3(unit.getMasterPlayerPosition()); 
+   self.player_distance = self.player_rel_pos:len();
+
+   self.altitude        = core.getAltitude();
+   self.atmosphere      = unit.getAtmosphereDensity();  -- [ 0..1]
+   self.planetinfluence = unit.getClosestPlanetInfluence(); -- [ 0..1]
+
+   local v_velo = vec3(core.getVelocity());
+   local speed  = v_velo:len();
+   self.kmh     = speed * 3.6;
+
+   if self.kmh > 1.0 then
+        v_velo.x=v_velo.x/speed;
+        v_velo.y=v_velo.y/speed;
+        v_velo.z=v_velo.z/speed;
+        self.v_angle_v = pi_half - acos(v_velo:dot(self.v_up)   );
+        self.v_angle_h = pi_half - acos(v_velo:dot(self.v_right));
+        self.v_angle_valid = true;
+   else
+        self.v_angle_v = 0.0;
+        self.v_angle_h = 0.0;
+        self.v_angle_valid = false;
+   end
+
+   
+   self.roll =gyro.getRoll();
+   self.pitch=gyro.getPitch();
+
+   if roll~=nil and pitch ~=nil then
+         self:CheckScreens(draw_10hz,draw_1hz);
+   end   
+
+end
+
+    -- ******************************************************************
+    --
+    -- ******************************************************************
+
+function FlyLib:CheckScreens(draw_10hz,draw_1hz)
+
 
 end
 
