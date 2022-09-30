ff5a80499790a0a88a468b18c0a4b6985e80827a
Olaf Reusch <apollo0510@hotmail.com>
2022-09-30 15:46:37 +0200

    haven monitor is nice

--- haven_monitor_display.lua
+++ haven_monitor_display.lua
@@ -1,11 +1,14 @@
 local json = require('dkjson');
+local format = string.format;
 local layer        = createLayer();
 local xres,yres    = getResolution();
-local font         = loadFont("Play",20);
+local font         = loadFont("Play",40);
+local font_small   = loadFont("Play",20);
 local column_width = 0;
 local row_height   = 0;
 local input_string = getInput();
 local padding      = 4;
+local text_pad     = 5;
 
 function GetCell(col,row,padding)
 	return { x0=(col-1)*column_width + padding,
@@ -20,37 +23,54 @@ if(type(input_string)=="string") then
 	local input        = json.decode(input_string);
 	if(type(input)=="table") then
 		if( (input.columns~=nil) and (input.rows~=nil) ) then
-			column_width = xres/input.columns;
-			row_height   = yres/input.rows;
 
-			setDefaultFillColor  (layer,Shape_Box,0,0,0,0);
-			setDefaultStrokeColor(layer,Shape_Box,1,1,1,1);
-			setDefaultStrokeWidth(layer,Shape_Box,2);
+			if input.columns>0 and input.rows>0 then
+
+				column_width = xres/input.columns;
+				row_height   = yres/input.rows;
+
+				setDefaultFillColor  (layer,Shape_Box,0,0,0,0);
+				setDefaultStrokeColor(layer,Shape_Box,1,1,1,1);
+				setDefaultStrokeWidth(layer,Shape_Box,2);
 		
-			local cell;
-			for col=1,input.columns do
-				for row=1,input.rows do
-					cell = GetCell(col,row,padding);
-					addBox(layer,cell.x0,cell.y0,cell.w,cell.h);
+				local cell;
+				for col=1,input.columns do
+					for row=1,input.rows do
+						cell = GetCell(col,row,padding);
+						addBox(layer,cell.x0,cell.y0,cell.w,cell.h);
+					end
 				end
-			end
 
-			setDefaultFillColor  (layer,Shape_Box,0,0.5,0,1);
-			setDefaultStrokeColor(layer,Shape_Box,0,0,0,0);
-			setDefaultStrokeWidth(layer,Shape_Box,0);
-			setDefaultTextAlign(layer, AlignH_Center, AlignV_Middle);
+				setDefaultFillColor  (layer,Shape_Box,0,0,0.3,1);
+				setDefaultStrokeColor(layer,Shape_Box,0,0,0,0);
+				setDefaultStrokeWidth(layer,Shape_Box,0);
+				setDefaultTextAlign(layer, AlignH_Center, AlignV_Middle);
 
-			if input.bars then
-				for i,bar in ipairs(input.bars) do
-					cell = GetCell(bar.col,bar.row,padding+2);
-					local w=cell.w*bar.perc/100;
-					addBox(layer,cell.x0,cell.y0,w,cell.h);
-					local x=cell.x0+cell.w/2;
-					local y=cell.y0+cell.h/2;
-					addText(layer,font,bar.name,x,y);
+				if input.bars then
+					for i,bar in ipairs(input.bars) do
+						cell = GetCell(bar.col,bar.row,padding+2);
+						local w=cell.w*bar.perc/100;
+						addBox(layer,cell.x0,cell.y0,w,cell.h);
+						local x=cell.x0+cell.w/2;
+						local y=cell.y0+cell.h/2;
+					
+						setNextTextAlign(layer, AlignH_Center, AlignV_Middle);
+						addText(layer,font,bar.name,x,y);
+					
+						local text=format("%.2f%%",bar.perc);
+						setNextTextAlign(layer, AlignH_Left, AlignV_Top);
+						addText(layer,font_small,text,cell.x0+text_pad,cell.y0+text_pad);
+
+						local text=format("%.3fkL",bar.vol/1000.0);
+						setNextTextAlign(layer, AlignH_Left, AlignV_Bottom);
+						addText(layer,font_small,text,cell.x0+text_pad,cell.y1-text_pad);
+
+					end
+				else
+					logMessage("Bars definitions missing");
 				end
 			else
-				logMessage("Bars definitions missing");
+				addText(layer,font,"Offline",xres/2,yres/2);
 			end
 		else
 			logMessage("Cols and Rows missing");
--- haven_monitor_pb.lua
+++ haven_monitor_pb.lua
@@ -2,28 +2,81 @@ local json = require('dkjson');
 local format = string.format;
 local print  = system.print;
 
+local display_data=
+{
+	columns=2;
+	rows   =4;
+
+	bars=
+	{
+		{ col=1,row=1,perc=0,vol=0,name="OreIn" },
+		{ col=1,row=2,perc=0,vol=0,name="T1Mix" },
+		{ col=2,row=1,perc=0,vol=0,name="Al_Fe" },
+		{ col=2,row=2,perc=0,vol=0,name="Silumin" },
+		{ col=2,row=3,perc=0,vol=0,name="Steel" },
+		{ col=2,row=4,perc=0,vol=0,name="Polycarbonate" },
+		{ col=1,row=4,perc=0,vol=0,name="Glas" },
+		{ col=1,row=3,perc=0,vol=0,name="Nitron" },
+	};
+};
+
+
+local container_data=
+{
+	{ cont=cont1; bar=display_data.bars[1]; volume=-1; },
+	{ cont=cont2; bar=display_data.bars[2]; volume=-1; },
+	{ cont=cont3; bar=display_data.bars[3]; volume=-1; },
+	{ cont=cont4; bar=display_data.bars[4]; volume=-1; },
+	{ cont=cont5; bar=display_data.bars[5]; volume=-1; },
+	{ cont=cont6; bar=display_data.bars[6]; volume=-1; },
+	{ cont=cont7; bar=display_data.bars[7]; volume=-1; },
+	{ cont=cont8; bar=display_data.bars[8]; volume=-1; },
+
+};
+
+
+function UpdateContainers()
+	local changed_false;
+	for i,c in ipairs(container_data) do
+		if c.cont then
+			local volume = c.cont.getItemsVolume();
+			if volume~=nil and volume~=c.volume then
+				c.volume=volume;
+				if c.max_volume == nil then
+					c.max_volume=c.cont.getMaxVolume();
+				end
+				local percent = c.volume * 100.0 / c.max_volume;
+				local bar     = c.bar;
+				if percent~=bar.perc then
+					bar.perc=percent;
+					bar.vol =volume;
+					changed=true;
+				end
+			end
+		end
+	end
+	if changed then
+		display.setScriptInput(json.encode(display_data));
+	end
+end
+
+local shutown_display_data=
+{
+	columns=0;
+	rows   =0;
+};
+
+local shutdown_string = json.encode(shutown_display_data);
+
+function Shutdown()
+	if display~=nil then
+		display.setScriptInput(shutdown_string);
+	end
+end
 
 if display~=nil then
-
-	local display_data=
-	{
-		columns=2;
-		rows   =4;
-
-		bars=
-		{
-			{ col=1,row=1,perc=0,name="OreIn" },
-			{ col=1,row=2,perc=0,name="T1Mix" },
-			{ col=2,row=1,perc=0,name="Al_Fe" },
-			{ col=2,row=2,perc=0,name="Silumin" },
-			{ col=2,row=3,perc=0,name="Steel" },
-			{ col=2,row=4,perc=0,name="Polycarbonate" },
-			{ col=1,row=4,perc=0,name="Glas" },
-			{ col=1,row=3,perc=0,name="Nitron" },
-		};
-	};
-
-	display.setScriptInput(json.encode(display_data));
+	UpdateContainers();
+	unit.setTimer("ContChecker",1.0);
 else
 	print("No display found");
 end
