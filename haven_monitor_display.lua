a29ed3599cf14695167c5ed038fc484ba5529ee4
Olaf Reusch <apollo0510@hotmail.com>
2022-10-24 07:12:09 +0200

    working on monitor

--- haven_monitor_display.lua
+++ haven_monitor_display.lua
@@ -10,6 +10,16 @@ local input_string = getInput();
 local padding      = 4;
 local text_pad     = 5;
 
+-- *****************************************************************
+local data_index_col        =1;
+local data_index_row        =2;
+local data_index_perc       =3;
+local data_index_vol        =4;
+local data_index_name       =5;
+local data_index_blueprints =6;
+-- *****************************************************************
+
+
 function GetCell(col,row,padding)
 	return { x0=(col-1)*column_width + padding,
 		    x1=(col-0)*column_width - padding,
@@ -48,22 +58,29 @@ if(type(input_string)=="string") then
 
 				if input.bars then
 					for i,bar in ipairs(input.bars) do
-						cell = GetCell(bar.col,bar.row,padding+2);
-						local w=cell.w*bar.perc/100;
+						cell = GetCell(bar[data_index_col],bar[data_index_row],padding+2);
+						local w=cell.w*bar[data_index_perc]/100;
 						addBox(layer,cell.x0,cell.y0,w,cell.h);
 						local x=cell.x0+cell.w/2;
 						local y=cell.y0+cell.h/2;
 					
 						setNextTextAlign(layer, AlignH_Center, AlignV_Middle);
-						addText(layer,font,bar.name,x,y);
+						addText(layer,font,bar[data_index_name],x,y);
 					
-						local text=format("%.2f%%",bar.perc);
+						local text=format("%.2f%%",bar[data_index_perc]);
 						setNextTextAlign(layer, AlignH_Left, AlignV_Top);
 						addText(layer,font_small,text,cell.x0+text_pad,cell.y0+text_pad);
 
-						local text=format("%.3fkL",bar.vol/1000.0);
+						local text=format("%.3fkL",bar[data_index_vol]/1000.0);
 						setNextTextAlign(layer, AlignH_Left, AlignV_Bottom);
 						addText(layer,font_small,text,cell.x0+text_pad,cell.y1-text_pad);
+
+						local blueprints=bar[data_index_blueprints];
+						if blueprints~=nil then
+							local text=format("%d",blueprints);
+							setNextTextAlign(layer, AlignH_Right, AlignV_Bottom);
+							addText(layer,font_small,text,cell.x1-text_pad,cell.y1-text_pad);
+						end
 
 					end
 				else
--- haven_monitor_pb.lua
+++ haven_monitor_pb.lua
@@ -1,45 +1,91 @@
 local json = require('dkjson');
 local format = string.format;
 local print  = system.print;
+local insert = table.insert;
+
+-- *****************************************************************
+local data_index_col       =1;
+local data_index_row        =2;
+local data_index_perc       =3;
+local data_index_vol        =4;
+local data_index_name       =5;
+local data_index_blueprints =6;
+-- *****************************************************************
+
 
 local display_data=
 {
 	columns=2;
 	rows   =4;
 
+	-- left display
 	bars=
 	{
-		{ col=1,row=1,perc=0,vol=0,name="OreIn" },
-		{ col=1,row=2,perc=0,vol=0,name="T1Mix" },
-		{ col=2,row=1,perc=0,vol=0,name="Al_Fe" },
-		{ col=2,row=2,perc=0,vol=0,name="Silumin" },
-		{ col=2,row=3,perc=0,vol=0,name="Steel" },
-		{ col=2,row=4,perc=0,vol=0,name="Polycarbonate" },
-		{ col=1,row=4,perc=0,vol=0,name="Glas" },
-		{ col=1,row=3,perc=0,vol=0,name="Nitron" },
+		{ 1,1,0,0,"Aluminium",nil},
+		{ 1,2,0,0,"Carbon"   ,nil},
+		{ 1,3,0,0,"Iron"     ,nil},
+		{ 1,4,0,0,"Silicon"  ,nil},
+		{ 2,1,0,0,"Calcium"  ,nil},
+		{ 2,2,0,0,"Chromium" ,nil},
+		{ 2,3,0,0,"Copper"   ,nil},
+		{ 2,4,0,0,"Sodium"   ,nil}
 	};
+
+	-- right display
+	--[[
+	bars=
+	{
+		{ 1,1,0,0,"Al-Fe"        ,nil},
+		{ 1,2,0,0,"Steel"        ,nil},
+		{ 1,3,0,0,"Silumin"      ,nil},
+		{ 1,4,0,0,"Polycarbonate",nil},
+		{ 2,1,0,0,"Calc.Reinf."  ,nil},
+		{ 2,2,0,0,"Stainless"    ,nil},
+		{ 2,3,0,0,"Duralumin"    ,nil},
+		{ 2,4,0,0,"PolyCalcide"  ,nil}
+	};
+	]]--
+
 };
 
 
 local container_data=
 {
-	{ cont=cont1; bar=display_data.bars[1]; volume=-1; },
-	{ cont=cont2; bar=display_data.bars[2]; volume=-1; },
-	{ cont=cont3; bar=display_data.bars[3]; volume=-1; },
-	{ cont=cont4; bar=display_data.bars[4]; volume=-1; },
-	{ cont=cont5; bar=display_data.bars[5]; volume=-1; },
-	{ cont=cont6; bar=display_data.bars[6]; volume=-1; },
-	{ cont=cont7; bar=display_data.bars[7]; volume=-1; },
-	{ cont=cont8; bar=display_data.bars[8]; volume=-1; },
-
+	{ cont=cont1; bar=display_data.bars[1]; volume=-1; schematicsRemaining=nil; },
+	{ cont=cont2; bar=display_data.bars[2]; volume=-1; schematicsRemaining=nil; },
+	{ cont=cont3; bar=display_data.bars[3]; volume=-1; schematicsRemaining=nil; },
+	{ cont=cont4; bar=display_data.bars[4]; volume=-1; schematicsRemaining=nil; },
+	{ cont=cont5; bar=display_data.bars[5]; volume=-1; schematicsRemaining=nil; },
+	{ cont=cont6; bar=display_data.bars[6]; volume=-1; schematicsRemaining=nil; },
+	{ cont=cont7; bar=display_data.bars[7]; volume=-1; schematicsRemaining=nil; },
+	{ cont=cont8; bar=display_data.bars[8]; volume=-1; schematicsRemaining=nil; }
 };
-
 
 function UpdateContainers()
 	local changed_false;
 	for i,c in ipairs(container_data) do
 		if c.cont then
 			local volume = c.cont.getItemsVolume();
+		    local schematicsRemaining = nil;
+			if c.ids then
+				for j,local_id in ipairs(c.ids) do
+				   local info=core.getElementIndustryInfoById(local_id);
+				   if info then
+					   if schematicsRemaining==nil or schematicsRemaining>info.schematicsRemaining then
+							schematicsRemaining=info.schematicsRemaining;
+  						    print("schematicsRemaining="..info.schematicsRemaining .. " id=" .. local_id);
+					   end
+				   end
+				end
+			end
+			if schematicsRemaining~=c.schematicsRemaining then
+				c.schematicsRemaining=schematicsRemaining;
+				local bar     = c.bar;
+				if bar then
+				   bar[data_index_blueprints]=schematicsRemaining;
+				   changed=true;
+				end
+			end
 			if volume~=nil and volume~=c.volume then
 				c.volume=volume;
 				if c.max_volume == nil then
@@ -47,9 +93,9 @@ function UpdateContainers()
 				end
 				local percent = c.volume * 100.0 / c.max_volume;
 				local bar     = c.bar;
-				if percent~=bar.perc then
-					bar.perc=percent;
-					bar.vol =volume;
+				if percent~=bar[data_index_perc] then
+					bar[data_index_perc]=percent;
+					bar[data_index_vol ] =volume;
 					changed=true;
 				end
 			end
@@ -59,6 +105,45 @@ function UpdateContainers()
 		display.setScriptInput(json.encode(display_data));
 	end
 end
+
+-- ****************************************************************
+
+function StartUp()
+
+	local IndustryTypes=
+	{
+		IndustryUnit=true,
+		Industry1   =true,
+		Industry2   =true,
+		Industry3   =true,
+		Industry4   =true,
+		Industry5   =true
+	};
+
+	local IndustryNames={ };
+	for i,c in ipairs(container_data) do
+		if c.cont then
+			local bar = c.bar;
+			local name="Ind_"..bar[data_index_name];
+			IndustryNames[name]=c;
+		end
+	end
+
+	local id_list=core.getElementIdList();
+	for i,local_id in ipairs(id_list) do
+		local class_name=core.getElementClassById(local_id);
+		if IndustryTypes[class_name] then
+			local element_name=core.getElementNameById(local_id);
+			local c=IndustryNames[element_name];
+			if c then
+			    if c.ids == nil then c.ids={}; end 
+			    insert(c.ids,local_id);
+			end
+		end
+	end
+end
+
+-- ****************************************************************
 
 local shutown_display_data=
 {
@@ -75,6 +160,7 @@ function Shutdown()
 end
 
 if display~=nil then
+	StartUp();
 	UpdateContainers();
 	unit.setTimer("ContChecker",1.0);
 else
